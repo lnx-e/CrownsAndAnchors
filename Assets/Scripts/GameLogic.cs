8597f5e173f521713faa397ffe8be6b3c49e2ebf
Megh-Menghani <109390815+Megh-Menghani@users.noreply.github.com>
2022-07-17 20:34:09 +0530

    more sounds
    added-
    diceSound
    epicWin
    win

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -13,7 +13,7 @@ public class GameLogic : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        playerCoins = 900;
+        playerCoins = 5;
     }
 
     // Update is called once per frame
